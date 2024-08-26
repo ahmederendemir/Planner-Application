7d9dd08f0871879ead4ae614683299fb932b7061
Ahmed Eren Demir <2003ahmederen@gmail.com>
2024-08-26 16:24:30 +0300

    Planner
    This Python-based planner application is designed to help users efficiently manage and organize their tasks. With a focus on user experience and customization, the application allows users to create, edit, and track their plans with ease.

---
+++ Daily Planner/planner.py
@@ -0,0 +1,103 @@
+from PyQt5.QtWidgets import QWidget,QApplication,QListWidgetItem,QMessageBox
+from PyQt5.uic import loadUi
+import sys
+from PyQt5.QtCore import Qt
+import sqlite3
+tasks=["write email","read a book"]
+class Application(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi("planner.ui",self)
+        self.calendarWidget.selectionChanged.connect(self.datechanged)
+        self.SaveButton.clicked.connect(self.savebutton)
+        self.AddTaskButton.clicked.connect(self.addtask)
+        self.DeleteButton.clicked.connect(self.delete)
+
+        
+    def updateTasks(self,date):
+        self.listWidget.clear()
+        db=sqlite3.connect("datab.db")
+        cursor=db.cursor()
+        query="SELECT Task,Completed FROM db WHERE Date=?"
+        where=(date,)
+        result=cursor.execute(query,where).fetchall()
+
+        for i in result:
+            item=QListWidgetItem(i[0])
+            if i[1] == "YES":
+                item.setCheckState(Qt.Checked)
+            elif i[1] == "NO":
+                item.setCheckState(Qt.Unchecked)
+            self.listWidget.addItem(item)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+
+    def datechanged(self):
+        
+        datei=self.calendarWidget.selectedDate().toPyDate()
+        self.updateTasks(datei)
+    
+    def savebutton(self):
+        datei=self.calendarWidget.selectedDate().toPyDate()
+        db=sqlite3.connect("datab.db")
+        cursor=db.cursor()
+
+        for i in range(self.listWidget.count()):
+            item=self.listWidget.item(i)
+            task=item.text()
+            if item.checkState()==Qt.Checked:
+                query="UPDATE db SET Completed='YES' WHERE Task=? AND Date=?"
+            else:
+                query="UPDATE db SET Completed='NO' WHERE Task=? AND Date=?"
+            row=(task,datei,)
+            cursor.execute(query,row)
+            db.commit()
+
+        messageBox = QMessageBox()
+        messageBox. setText("Changes saved.")
+        messageBox.setStandardButtons (QMessageBox.Ok)
+        messageBox. exec()
+    def addtask(self):
+        db = sqlite3.connect("datab.db")
+        cursor = db. cursor()
+        new = str(self.lineEdit.text())
+        datei=self.calendarWidget.selectedDate().toPyDate()
+        query = "INSERT INTO db(Task, Completed, Date) VALUES (?,?,?)"
+        row = (new, "NO", datei, )
+        cursor.execute(query, row)
+        db. commit()
+        self.lineEdit.clear()
+        self.updateTasks(datei)
+
+        messageBox = QMessageBox()
+        messageBox. setText("Changes saved.")
+        messageBox.setStandardButtons (QMessageBox.Ok)
+        messageBox. exec()
+    
+    def delete(self):
+        db = sqlite3.connect("datab.db")
+        cursor = db. cursor()
+        datei=self.calendarWidget.selectedDate().toPyDate()
+        selectedTask=self.listWidget.currentItem().text()
+        query="DELETE FROM db WHERE Task=?"
+        row=(selectedTask,)
+        cursor.execute(query, row)
+        db. commit()
+        self.updateTasks(datei)
+
+        messageBox = QMessageBox()
+        messageBox. setText("Changes saved.")
+        messageBox.setStandardButtons (QMessageBox.Ok)
+        messageBox. exec()
+
+        
+        
+        
+        
+        
+    
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    application=Application()
+    application.show()
+    sys.exit(app.exec())
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Planner Application
- Planner for your daily tasks
+Planner Application
+This Python-based planner application is designed to help users efficiently manage and organize their tasks. With a focus on user experience and customization, the application allows users to create, edit, and track their plans with ease.
+
+Features:
+--User-Friendly Interface: Simple and intuitive design for effortless navigation.
+--Task Management: Create, edit, and delete tasks quickly and easily.
+--Personalized Planning: Customize your planning experience to suit your needs.
+
